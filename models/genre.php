f5df5c30892991c27c1f6dcd8637d7683b46150e
GiuseppeArgo <giuseppe.argo18@gmail.com>
2024-06-02 17:52:51 +0200

    feat: genre in genre.php

--- models/genre.php
+++ models/genre.php
@@ -1,1 +1,13 @@
 <?php
+
+class Genre{
+    public string $name;
+    public string $recommended;
+
+    public function __construct(string $name, string $recommended) {
+
+        $this->name         = $name;
+        $this->recommended   = $recommended;
+    }
+}
+?>
