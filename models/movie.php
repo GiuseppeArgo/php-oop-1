0c3a0bf2b9cbd3291bac0f0d63741aa424ef996f
GiuseppeArgo <giuseppe.argo18@gmail.com>
2024-06-02 18:00:14 +0200

    feat: movie in movie.php

--- models/movie.php
+++ models/movie.php
@@ -1,2 +1,16 @@
 <?php
+class Movie{
+    public string $title;
+    public string $description;
+    public int $vote;
+    public string $release_date;
+    public Genre $genre; 
 
+    public function __construct(string $title, Genre $genre) {
+
+        $this->title    = $title;
+        $this->genre    = $genre;
+    }
+}
+
+?>
